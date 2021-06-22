bc2a9853f9b0d1b323b557272567806e8d2ffb99
VolobuevEA <VolobuevEA@samnipi.rosneft.ru>
2021-06-22 13:09:05 +0400

    Добавьте файлы проекта.

---
+++ ClothingApp/Models/Weather.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothingApp.Models
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// температура
+        /// </summary>
+        public int Temperature { get; set; }
+        /// <summary>
+        /// скорость ветра
+        /// </summary>
+        public double Wind { get; set; }
+        /// <summary>
+        /// снег
+        /// </summary>
+        public bool Snow { get; set; }
+        /// <summary>
+        /// дождь
+        /// </summary>
+        public bool Rain { get; set; }
+        /// <summary>
+        /// ясно
+        /// </summary>
+        public bool Clear { get; set; }
+        /// <summary>
+        /// пасмурно
+        /// </summary>
+        public bool Сloudy { get; set; }
+    }
+}
---
+++ ClothingApp/Models/Сlothes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothingApp.Models
+{
+    public class Сlothes
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// верх 
+        /// </summary>
+        public string BodyClothes { get; set; }
+        /// <summary>
+        /// низ
+        /// </summary>
+        public string LegsClothes { get; set; }
+        /// <summary>
+        /// обувь
+        /// </summary>
+        public string Shoes { get; set; }
+        
+    }
+}
