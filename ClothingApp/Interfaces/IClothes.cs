d49bab75076b505d101501c8cd94891dd074c8d7
GorshkovaTatiana <79208495+GorshkovaTatiana@users.noreply.github.com>
2021-06-22 16:38:29 +0400

    Add Interfaces

---
+++ ClothingApp/Interfaces/IClothes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothingApp.Interfaces
+{
+    interface IClothes
+    {
+        /// <summary>
+        /// получение из БД одежду/образ(шаблон)
+        /// </summary>
+        void GetClothes();
+        /// <summary>
+        /// изменение из БД одежды/образа(шаблона)
+        /// </summary>
+        void ChangeClothes();
+        /// <summary>
+        /// удаление из БД одежды/образа(шаблона)
+        /// </summary>
+        void DeleteClothes();
+        
+
+    }
+}
