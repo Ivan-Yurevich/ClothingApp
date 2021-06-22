4c2f6143a1b07b4705553a4f6281058e25a1cf2b
Anastasia Nenasheva <70859848+anastasia-nenash@users.noreply.github.com>
2021-06-22 18:32:42 +0300

    Revert "Si 2(clothes)"

--- ClothingApp/Models/Сlothes.cs
+++ ClothingApp/Models/Сlothes.cs
@@ -5,75 +5,21 @@ using System.Threading.Tasks;
 
 namespace ClothingApp.Models
 {
-    public abstract class Сlothes
+    public class Сlothes
     {
         public int Id { get; set; }
-
         /// <summary>
-        /// пол (мужской, женский)
+        /// верх 
         /// </summary>
-        public string Gender { get; set; }
-        public string Url { get; set; }
-
+        public string BodyClothes { get; set; }
         /// <summary>
-        /// диапазон температуры, в котором стоит носить эту одежду
+        /// низ
         /// </summary>
-        public int MinTemp { get; set; }
-        public int MaxTemp { get; set; }
-
+        public string LegsClothes { get; set; }
         /// <summary>
-        /// можно ли надеть в дождь? 
+        /// обувь
         /// </summary>
-        public bool IsforRain { get; set; }
-
-        /// <summary>
-        /// совет по модели, цвету
-        /// </summary>
-        public string Advice { get; set; }
-
-    }
-
-    /// <summary>
-    /// обувь
-    /// </summary>
-    public class Shoes : Сlothes
-    {
-
-    }
-
-    /// <summary>
-    /// низ
-    /// штаны/шорты/юбки
-    /// </summary>
-    public class LegsClothes : Сlothes
-    {
-
-    }
-
-    /// <summary>
-    /// верхняя одежда
-    /// куртки, пальто, и т.д.
-    /// </summary>
-    public class OutWear : Сlothes
-    {
-
-    }
-
-    /// <summary>
-    /// верх
-    /// футболка, рубашка, свитер и т.д.
-    /// </summary>
-    public class BodyClothes : Сlothes
-    {
-
-    }
-
-    /// <summary>
-    /// аксессуары
-    /// зонты, шарфы, шляпы, шапки и т.д.
-    /// </summary>
-    public class Accessory : Сlothes
-    {
-
+        public string Shoes { get; set; }
+        
     }
 }
