276ad55cb28d7998538ed518b2444a1bda562646
llviktoriall <victoria_19.01.2000@mail.ru>
2021-06-28 16:02:09 +0400

    Метод для определения местоположения текущего пользователя и частичное представление.

--- ClothingApp/Controllers/WeatherController.cs
+++ ClothingApp/Controllers/WeatherController.cs
@@ -1,5 +1,7 @@
 ﻿using ClothingApp.Core.Services.WeatherService;
 using ClothingApp.Data.Common.Models;
+using ClothingApp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,10 +15,13 @@ namespace ClothingApp.Web.Controllers
     public class WeatherController : Controller
     {
         private readonly IWeatherService _weatherService;
+        UserManager<ApplicationUser> _userManager;
+        private string userCity;
 
-        public WeatherController(IWeatherService weatherService)
+        public WeatherController(IWeatherService weatherService, UserManager<ApplicationUser> userManager)
         {
             _weatherService = weatherService;
+            _userManager = userManager;
         }
 
         private string GetCityByIP()
@@ -24,6 +29,17 @@ namespace ClothingApp.Web.Controllers
             var remoteIpAddress = HttpContext.Connection.RemoteIpAddress.ToString();
 
             return _weatherService.GetCityName("46.0.40.18"); // ip adress
+        }
+
+        private async void GetCityFromUser()
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                ApplicationUser user = await _userManager.FindByNameAsync(this.User.Identity.Name);
+                userCity =  user.City;
+            }
+            else userCity = "";
+
         }
         /// <summary>
         /// погода на 5 дней
@@ -64,13 +80,25 @@ namespace ClothingApp.Web.Controllers
         }
         [HttpGet]
         public ActionResult Today()
-        {           
+        {
+            GetCityFromUser();
             ViewBag.GetWeatherToday = GetWeatherToday();
+            ViewBag.IsDifferentPlaces = false;
+            if (GetCityByIP()!=userCity&&userCity!="")
+            {
+                ViewBag.IsDifferentPlaces = true;
+            }
             return View("Today");
         }
         public ActionResult Tomorrow()
         {
+            GetCityFromUser();
             ViewBag.GetWeatherTomorrow = GetWeatherTomorrow();
+            ViewBag.IsDifferentPlaces = false;
+            if (GetCityByIP() != userCity && userCity != "")
+            {
+                ViewBag.IsDifferentPlaces = true;
+            }
             return View("Tomorrow");
         }
     }
